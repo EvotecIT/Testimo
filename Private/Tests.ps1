fac14a8dbc229b2e0e03e986609f1a9df897abd6
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-05 15:49:42 +0200

    Update

--- Private/Tests.ps1
+++ Private/Tests.ps1
@@ -1,11 +1,34 @@
 ﻿$Script:Tests = [ordered] @{
-    Forest = @{
-
+    Forest            = @{
+        Tests = [ordered]  @{
+            OptionalFeatures = @{
+                Enable      = $true
+                ScriptBlock = $Script:SBForestOptionalFeatures
+            }
+            Replication      = @{
+                Enable      = $false
+                ScriptBlock = $Script:SBForestReplication
+            }
+            LastBackup       = @{
+                Enable      = $true
+                ScriptBlock = $Script:SBForestLastBackup
+            }
+        }
     }
-    Domain = @{
-
+    Domain            = @{
+        Tests = [ordered] @{
+            PasswordComplexity = @{
+                Enable      = $true
+                ScriptBlock = $Script:SBDomainPasswordComplexity
+            }
+        }
     }
     DomainControllers = @{
-
+        Tests = [ordered] @{
+            LDAP = @{
+                Enable      = $true
+                ScriptBlock = $Script:SBDomainControllersLDAP
+            }
+        }
     }
 }
