3a59a3ce30a87bf1568f4dcc41876db71ba6013b
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-09 22:50:04 +0200

    Update

--- Example/Example-TestActiveDirectory.ps1
+++ Example/Example-TestActiveDirectory.ps1
@@ -1,4 +1,4 @@
 ﻿Import-Module .\Testimo.psd1 -Force #-Verbose
 
-$TestResults = Invoke-Testimo -ExcludeDomains 'ad.evotec.pl' -ExcludeDomainControllers 'ADRODC.ad.evotec.pl' -ReturnResults -Sources DCPorts,DCNetworkCardSettings
+$TestResults = Invoke-Testimo -Sources DomainDNSZonesDomain0ADEL,DomainDNSZonesForest0ADEL -ShowReport # -ExcludeDomains 'ad.evotec.pl' -ExcludeDomainControllers 'ADRODC.ad.evotec.pl' -ReturnResults
 $TestResults | Format-Table -AutoSize *
