4f04ff11b4c3b1ec3a3978ce2670b8eeb49b5f6a
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-09 22:49:54 +0200

    #31

--- Private/SourcesDomain/DNSZonesForest0ADEL.ps1
+++ Private/SourcesDomain/DNSZonesForest0ADEL.ps1
@@ -13,14 +13,16 @@
             IsDomainRoot = $true
         }
         Details      = [ordered] @{
-            Area        = ''
-            Category    = ''
+            Area        = 'Configuration'
+            Category    = 'DNS'
             Severity    = ''
             RiskLevel   = 0
             Description = ""
             Resolution  = ''
             Resources   = @(
-
+                'https://blogs.technet.microsoft.com/the_9z_by_chris_davis/2011/12/20/forestdnszones-or-domaindnszones-fsmo-says-the-role-owner-attribute-could-not-be-read/'
+                'https://support.microsoft.com/en-us/help/949257/error-message-when-you-run-the-adprep-rodcprep-command-in-windows-serv'
+                'https://social.technet.microsoft.com/Forums/en-US/8b4a7794-13b2-4ef0-90c8-16799e9fd529/orphaned-fsmoroleowner-entry-for-domaindnszones?forum=winserverDS'
             )
 
         }
