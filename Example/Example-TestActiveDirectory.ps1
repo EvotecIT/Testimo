bf9105d65a39b55d0ee5aacdf5e8ae69f49b0aa4
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-12 20:15:06 +0200

    Update examples

--- Example/Example-TestActiveDirectory.ps1
+++ Example/Example-TestActiveDirectory.ps1
@@ -1,4 +1,4 @@
 ﻿Import-Module .\Testimo.psd1 -Force #-Verbose
 
-$TestResults = Invoke-Testimo -Sources DomainDNSZonesDomain0ADEL,DomainDNSZonesForest0ADEL -ShowReport # -ExcludeDomains 'ad.evotec.pl' -ExcludeDomainControllers 'ADRODC.ad.evotec.pl' -ReturnResults
+$TestResults = Invoke-Testimo -Sources DomainDNSZonesDomain0ADEL,DomainDNSZonesForest0ADEL,DCEventLogs #-ShowReport # -ExcludeDomains 'ad.evotec.pl' -ExcludeDomainControllers 'ADRODC.ad.evotec.pl' -ReturnResults
 $TestResults | Format-Table -AutoSize *
