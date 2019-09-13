b1627c79eca6e14cef677df325efb70fad2bfa40
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-13 15:11:19 +0200

    Update

--- Example/Example-TestActiveDirectory.ps1
+++ Example/Example-TestActiveDirectory.ps1
@@ -1,4 +1,4 @@
 ﻿Import-Module .\Testimo.psd1 -Force #-Verbose
 
-$TestResults = Invoke-Testimo -Sources DomainDNSZonesDomain0ADEL,DomainDNSZonesForest0ADEL,DCEventLogs #-ShowReport # -ExcludeDomains 'ad.evotec.pl' -ExcludeDomainControllers 'ADRODC.ad.evotec.pl' -ReturnResults
+$TestResults = Invoke-Testimo -Sources DCTimeSynchronizationExternal -ShowReport -ExcludeDomains 'ad.evotec.pl' #, ForestBackup, DomainDNSZonesDomain0ADEL, DCEventLogs, ForestReplicationStatus #,DomainDNSZonesForest0ADEL,DCEventLogs#-ShowReport # -ExcludeDomains 'ad.evotec.pl' -ExcludeDomainControllers 'ADRODC.ad.evotec.pl' -ReturnResults
 $TestResults | Format-Table -AutoSize *
