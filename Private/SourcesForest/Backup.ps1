e15aa9f1b4ee4acf4f3c98d846c31837e70ad6e3
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-13 08:29:53 +0200

    Rename

---
+++ Private/SourcesForest/Backup.ps1
@@ -0,0 +1,31 @@
+﻿$Backup = @{
+    Enable = $true
+    Source = @{
+        Name = 'Forest Backup'
+        Data = {
+            Get-WinADLastBackup
+        }
+        Details = [ordered] @{
+            Area             = 'Backup'
+            Description      = ''
+            Resolution   = ''
+            RiskLevel        = 10
+            Resources = @(
+
+            )
+        }
+    }
+    Tests  = [ordered] @{
+        LastBackupTests = @{
+            Enable     = $true
+            Name       = 'Forest Last Backup Time - Context'
+            Parameters = @{
+                ExpectedValue         = 2
+                OperationType         = 'lt'
+                Property              = 'LastBackupDaysAgo'
+                PropertyExtendedValue = 'LastBackup'
+                OverwriteName         = { "Last Backup $($_.NamingContext)" }
+            }
+        }
+    }
+}
