6970a8b33e9c85a8084b8e2ef7df619d970b5c84
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-17 07:44:32 +0200

    Update

--- Private/SourcesDomainControllers/TimeSynchronizationInternal.ps1
+++ Private/SourcesDomainControllers/TimeSynchronizationInternal.ps1
@@ -16,6 +16,15 @@
         }
     }
     Tests              = [ordered] @{
+        LastBootUpTime = @{
+            Enable     = $true
+            Name       = 'Last Boot Up time should be less than X days'
+            Parameters = @{
+                Property      = 'LastBootUpTime'
+                ExpectedValue = '(Get-Date).AddDays(-60)'
+                OperationType = 'gt'
+            }
+        }
         TimeSynchronizationTest = @{
             Enable     = $true
             Name       = 'Time Difference'
@@ -28,3 +37,11 @@
         }
     }
 }
+
+<#
+Name   LocalDateTime       RemoteDateTime      InstallTime         LastBootUpTime      TimeDifferenceMinutes TimeDifferenceSeconds TimeDifferenceMilliseconds TimeSourceName    Status
+----   -------------       --------------      -----------         --------------      --------------------- --------------------- -------------------------- --------------    ------
+AD2    17.09.2019 07:38:57 17.09.2019 07:38:57 30.05.2018 18:30:48 13.09.2019 07:54:10    0,0417166666666667                 2,503                       2503 AD1.ad.evotec.xyz
+AD3    17.09.2019 07:38:56 17.09.2019 02:38:57 26.05.2019 17:30:17 13.09.2019 07:54:09               0,02175                 1,305                       1305 AD1.ad.evotec.xyz
+EVOWin 17.09.2019 07:38:57 17.09.2019 07:38:57 24.05.2019 22:46:45 13.09.2019 07:53:44                0,0415                  2,49                       2490 AD1.ad.evotec.xyz
+#>
