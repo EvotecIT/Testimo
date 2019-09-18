6752bee052a9f4997cdafd91aa990c5a5f38e3e3
JasonCook599 <JasonCook599@users.noreply.github.com>
2019-09-18 13:07:59 -0400

    Empty organization units ignore new items ou
    The Empty Organization Units test should ignore the new users and new computers container.

--- Private/SourcesDomain/EmptyOrganizationalUnits.ps1
+++ Private/SourcesDomain/EmptyOrganizationalUnits.ps1
@@ -4,13 +4,14 @@
         Name           = "Orphaned/Empty Organizational Units"
         Data           = {
             $OrganizationalUnits = Get-ADOrganizationalUnit -Filter * -Properties distinguishedname -Server $Domain | Select-Object -ExpandProperty distinguishedname
+            $WellKnownContainers = Get-ADDomain | Select-Object *Container
 
             $AllUsedOU = Get-ADObject -Filter "ObjectClass -eq 'user' -or ObjectClass -eq 'computer' -or ObjectClass -eq 'group' -or ObjectClass -eq 'contact'" -Server $Domain | `
                 Where-Object { ($_.DistinguishedName -notlike '*LostAndFound*') -and ($_.DistinguishedName -match 'OU=(.*)') } | `
                 ForEach-Object { $matches[0] } | `
                 Select-Object -Unique
 
-            $OrganizationalUnits | Where-Object { ($AllUsedOU -notcontains $_) -and -not (Get-ADOrganizationalUnit -Filter * -SearchBase $_ -SearchScope 1 -Server $Domain) }
+            $OrganizationalUnits | Where-Object { ($AllUsedOU -notcontains $_) -and -not (Get-ADOrganizationalUnit -Filter * -SearchBase $_ -SearchScope 1 -Server $Domain) -and (($_ -notlike $WellKnownContainers.UsersContainer) -or ($_ -notlike $WellKnownContainers.ComputersContainer)) }
         }
         ExpectedOutput = $false
         Details = [ordered] @{
