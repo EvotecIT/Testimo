04bb4c73b80f7e11105fbb08a73ec1a32978d483
JasonCook599 <JasonCook599@users.noreply.github.com>
2019-09-18 13:06:58 -0400

    Revert "Empty organization units ignore new location"
    This reverts commit 5532e4c393fb32c465f52bd168739d1d4cc721e6.

--- Private/SourcesDomain/EmptyOrganizationalUnits.ps1
+++ Private/SourcesDomain/EmptyOrganizationalUnits.ps1
@@ -4,14 +4,13 @@
         Name           = "Orphaned/Empty Organizational Units"
         Data           = {
             $OrganizationalUnits = Get-ADOrganizationalUnit -Filter * -Properties distinguishedname -Server $Domain | Select-Object -ExpandProperty distinguishedname
-            $WellKnownContainers = Get-ADDomain | Select-Object *Container
 
             $AllUsedOU = Get-ADObject -Filter "ObjectClass -eq 'user' -or ObjectClass -eq 'computer' -or ObjectClass -eq 'group' -or ObjectClass -eq 'contact'" -Server $Domain | `
-                Where-Object { ($_.DistinguishedName -notlike '*get-LostAndFound*') -and ($_.DistinguishedName -match 'OU=(.*)') } | `
+                Where-Object { ($_.DistinguishedName -notlike '*LostAndFound*') -and ($_.DistinguishedName -match 'OU=(.*)') } | `
                 ForEach-Object { $matches[0] } | `
                 Select-Object -Unique
 
-            $OrganizationalUnits | Where-Object { ($AllUsedOU -notcontains $_) -and -not (Get-ADOrganizationalUnit -Filter * -SearchBase $_ -SearchScope 1 -Server $Domain) -and (($_ -notlike $WellKnownContainers.UsersContainer) -or ($_ -notlike $WellKnownContainers.ComputersContainer)) }
+            $OrganizationalUnits | Where-Object { ($AllUsedOU -notcontains $_) -and -not (Get-ADOrganizationalUnit -Filter * -SearchBase $_ -SearchScope 1 -Server $Domain) }
         }
         ExpectedOutput = $false
         Details = [ordered] @{
