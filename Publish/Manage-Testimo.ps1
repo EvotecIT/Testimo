962438175d8d106310f9ded72e1069fbfe05d051
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-03-14 17:40:51 +0100

    Published new version 0.38

--- Publish/Manage-Testimo.ps1
+++ Publish/Manage-Testimo.ps1
@@ -151,10 +151,10 @@ $Configuration = @{
         }
         BuildDocumentation = $false
         PublishModule      = @{
-            Enabled      = $false
+            Enabled      = $true
             Prerelease   = ''
             RequireForce = $false
-            GitHub       = $false
+            GitHub       = $true
         }
     }
 }
