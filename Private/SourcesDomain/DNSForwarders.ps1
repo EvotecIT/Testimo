2a7de5e2ae60067bf332aaba2d1268fda12bf9ea
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-03-18 23:58:53 +0100

    Modified to acoomodate new PSWinDocumentation.DNS

--- Private/SourcesDomain/DNSForwarders.ps1
+++ Private/SourcesDomain/DNSForwarders.ps1
@@ -3,21 +3,22 @@
     Source = @{
         Name    = "DNS Forwarders"
         Data    = {
-            #$PSWinDocumentationDNS = Import-Module PSWinDocumentation.DNS -PassThru
-            $PSWinDocumentationDNS = Import-PrivateModule PSWinDocumentation.DNS
-            & $PSWinDocumentationDNS {
-                param($Domain)
-                [Array] $Forwarders = Get-WinDnsServerForwarder -Domain $Domain -WarningAction SilentlyContinue
-                if ($Forwarders.Count -gt 1) {
-                    Compare-MultipleObjects -Objects $Forwarders -FormatOutput -CompareSorted:$true -ExcludeProperty GatheredFrom -SkipProperties -Property 'IpAddress' -WarningAction SilentlyContinue #| Out-HtmlView -ScrollX -DisablePaging  -Filtering
-                } else {
-                    # This code takes care of only 1 server within a domain. If there is 1 server available (as others may be dead/unavailable at the time it assumes Pass)
-                    [PSCustomObject] @{
-                        Source = $Forwarders[0].IPAddress -join ', '
-                        Status = $true
-                    }
+            [Array] $Forwarders = Get-WinDnsServerForwarder -Domain $Domain -WarningAction SilentlyContinue
+            if ($Forwarders.Count -gt 1) {
+                Compare-MultipleObjects -Objects $Forwarders -FormatOutput -CompareSorted:$true -ExcludeProperty GatheredFrom -SkipProperties -Property 'IpAddress' -WarningAction SilentlyContinue #| Out-HtmlView -ScrollX -DisablePaging  -Filtering
+            } elseif ($Forwarders.Count -eq 0) {
+                [PSCustomObject] @{
+                    # This code takes care of no forwarders
+                    Source = 'No forwarders set'
+                    Status = $false
                 }
-            } $Domain
+            } else {
+                # This code takes care of only 1 server within a domain. If there is 1 server available (as others may be dead/unavailable at the time it assumes Pass)
+                [PSCustomObject] @{
+                    Source = $Forwarders[0].IPAddress -join ', '
+                    Status = $true
+                }
+            }
         }
         Details = [ordered] @{
             Area        = 'Configuration'
