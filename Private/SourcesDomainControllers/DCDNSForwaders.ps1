c893b6ad3ed1a2ec1cad24d1567ea34d1cce0461
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-03-18 23:58:40 +0100

    Added DCDNSForwarders

---
+++ Private/SourcesDomainControllers/DCDNSForwaders.ps1
@@ -0,0 +1,34 @@
+﻿$DCDNSForwaders = @{
+    Enable = $true
+    Source = @{
+        Name    = "DC DNS Forwarders"
+        Data    = {
+            $Forwarders = Get-WinDnsServerForwarder -Domain $Domain -IncludeDomainControllers $DomainController -WarningAction SilentlyContinue -Formatted
+            $Forwarders
+        }
+        Details = [ordered] @{
+            Area        = 'Configuration'
+            Category    = ''
+            Severity    = ''
+            RiskLevel   = 0
+            Description = ''
+            Resolution  = ''
+            Resources   = @(
+
+            )
+        }
+    }
+    Tests  = [ordered] @{
+        SameForwarders = @{
+            Enable      = $true
+            Name        = 'Multiple DNS Forwarders'
+            Parameters  = @{
+                Property              = 'ForwardersCount'
+                ExpectedValue         = 1
+                OperationType         = 'gt'
+                PropertyExtendedValue = 'IPAddress'
+            }
+            Description = 'DNS: More than one forwarding server should be configured'
+        }
+    }
+}
