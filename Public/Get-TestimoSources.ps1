1310aedb4f2362c760e9a698284d8acdcaea5419
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-05-23 00:29:46 +0200

    Updated Get-TestimoSources

--- Public/Get-TestimoSources.ps1
+++ Public/Get-TestimoSources.ps1
@@ -1,50 +1,75 @@
 ﻿function Get-TestimoSources {
     [CmdletBinding()]
     param(
-        [string[]] $Source,
-        [switch] $Enabled
+        [string[]] $Sources,
+        [switch] $SourcesOnly,
+        [switch] $Enabled,
+        [switch] $Advanced
     )
-    if ($Source) {
-        $DetectedSource = ConvertTo-Source -Source $Source
-        $Scope = $DetectedSource.Scope
-        $Name = $DetectedSource.Name
-        $Script:TestimoConfiguration.$Scope[$Name].Tests.Keys
+    if (-not $Sources) {
+        $Sources = @(
+            $ForestKeys = $Script:TestimoConfiguration.Forest.Keys
+            $DomainKeys = $Script:TestimoConfiguration.Domain.Keys
+            $DomainControllerKeys = $Script:TestimoConfiguration.DomainControllers.Keys
 
-    } else {
-        $ForestKeys = $TestimoConfiguration.Forest.Keys
-        $DomainKeys = $TestimoConfiguration.Domain.Keys
-        $DomainControllerKeys = $TestimoConfiguration.DomainControllers.Keys
-
-        $TestSources = @(
-            foreach ($Key in $ForestKeys) {
-                if ($Enabled) {
-                    if ($TestimoConfiguration.Forest["$Key"].Enable) {
+            $TestSources = @(
+                foreach ($Key in $ForestKeys) {
+                    if ($Enabled) {
+                        if ($Script:TestimoConfiguration.Forest["$Key"].Enable) {
+                            "Forest$Key"
+                        }
+                    } else {
                         "Forest$Key"
                     }
-                } else {
-                    "Forest$Key"
-                }
 
-            }
-            foreach ($Key in $DomainKeys) {
-                if ($Enabled) {
-                    if ($TestimoConfiguration.Domain["$Key"].Enable) {
+                }
+                foreach ($Key in $DomainKeys) {
+                    if ($Enabled) {
+                        if ($Script:TestimoConfiguration.Domain["$Key"].Enable) {
+                            "Domain$Key"
+                        }
+                    } else {
                         "Domain$Key"
                     }
-                } else {
-                    "Domain$Key"
                 }
-            }
-            foreach ($Key in $DomainControllerKeys) {
-                if ($Enabled) {
-                    if ($TestimoConfiguration.DomainControllers["$Key"].Enable) {
+                foreach ($Key in $DomainControllerKeys) {
+                    if ($Enabled) {
+                        if ($Script:TestimoConfiguration.DomainControllers["$Key"].Enable) {
+                            "DC$Key"
+                        }
+                    } else {
                         "DC$Key"
                     }
-                } else {
-                    "DC$Key"
                 }
-            }
+            )
+            $TestSources | Sort-Object
         )
-        $TestSources | Sort-Object
+    }
+    if ($SourcesOnly) {
+        return $TestSources
+    }
+
+    foreach ($S in $Sources) {
+        $DetectedSource = ConvertTo-Source -Source $S
+        $Scope = $DetectedSource.Scope
+        $Name = $DetectedSource.Name
+        $Object = [ordered]@{
+            Source = $S
+            Scope  = $Scope
+            Name   = $Name
+            Tests  = $Script:TestimoConfiguration.$Scope[$Name].Tests.Keys
+        }
+        $Object['Area'] = $Script:TestimoConfiguration.$Scope[$Name].Source.Details.Area
+        $Object['Category'] = $Script:TestimoConfiguration.$Scope[$Name].Source.Details.Category
+        $Object['Severity'] = $Script:TestimoConfiguration.$Scope[$Name].Source.Details.Severity
+        $Object['RiskLevel'] = $Script:TestimoConfiguration.$Scope[$Name].Source.Details.RiskLevel
+        $Object['Description'] = $Script:TestimoConfiguration.$Scope[$Name].Source.Details.Description
+        $Object['Resolution'] = $Script:TestimoConfiguration.$Scope[$Name].Source.Details.Resolution
+        $Object['Resources'] = $Script:TestimoConfiguration.$Scope[$Name].Source.Details.Resources
+
+        if ($Advanced) {
+            $Object['Advanced'] = $Script:TestimoConfiguration.$Scope[$Name]
+        }
+        [PSCustomObject] $Object
     }
 }
