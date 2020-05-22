b45c99f9a5741488dd5433c5f78a87b0d86fa959
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-05-22 19:59:45 +0200

    Added ExpectedResult for Parameters

--- Private/Test-StepOne.ps1
+++ Private/Test-StepOne.ps1
@@ -13,7 +13,7 @@
         [nullable[int]] $ExpectedCount,
         [string] $OperationResult,
         [string] $ReferenceID,
-        #[nullable[bool]] $ExpectedOutput,
+        [nullable[bool]] $ExpectedResult,
         [nullable[bool]] $ExpectedOutput,
         [scriptblock] $WhereObject,
         [scriptblock] $OverwriteName,
@@ -36,7 +36,21 @@
             }
         }
 
+        # This checks for ExpectedResult/ExpectedOutput
+        # The difference is that
+        # - ExpectedResult (true/false) - when ExpectedResult is True it means we filtered our objected with Where and it still provided output
+        #   This allows us to not check each element in Array one by one, but just assume this in bundle
+        # - ExpectedOutput (true/false) - when we do Where-Object but it's possible that Array won't contain what we are looking for, and at the same time, it's not a problem
         if ($null -eq $Object) {
+            if ($ExpectedResult -eq $false) {
+                Out-Begin -Text $TestName -Level $Level -Domain $Domain -DomainController $DomainController
+                Out-Status -Text $TestName -Status $true -ExtendedValue "Data is not available. This is expected." -Domain $Domain -DomainController $DomainController -ReferenceID $ReferenceID
+                return $true
+            } elseif ($ExpectedResult -eq $true) {
+                Out-Begin -Text $TestName -Level $Level -Domain $Domain -DomainController $DomainController
+                Out-Status -Text $TestName -Status $false -ExtendedValue 'Data is not available. This is not expected.' -Domain $Domain -DomainController $DomainController -ReferenceID $ReferenceID
+                return $false
+            }
             # This checks for NULL after Where-Object
             # Data Source is not null, but after WHERE-Object becomes NULL - we need to fail this
             if ($null -eq $ExpectedOutput -or $ExpectedOutput -eq $true) {
@@ -46,6 +60,16 @@
             } else {
                 Out-Begin -Text $TestName -Level $Level -Domain $Domain -DomainController $DomainController
                 Out-Status -Text $TestName -Status $true -ExtendedValue "Data is not available, but it's not required." -Domain $Domain -DomainController $DomainController -ReferenceID $ReferenceID
+                return $true
+            }
+        } else {
+            if ($ExpectedResult -eq $false) {
+                Out-Begin -Text $TestName -Level $Level -Domain $Domain -DomainController $DomainController
+                Out-Status -Text $TestName -Status $false -ExtendedValue 'Data is available. This is not expected.' -Domain $Domain -DomainController $DomainController -ReferenceID $ReferenceID
+                return $false
+            } elseif ($ExpectedResult -eq $true) {
+                Out-Begin -Text $TestName -Level $Level -Domain $Domain -DomainController $DomainController
+                Out-Status -Text $TestName -Status $true -ExtendedValue "Data is available. This is expected." -Domain $Domain -DomainController $DomainController -ReferenceID $ReferenceID
                 return $true
             }
         }
