641f79bdf0f5696835760114e141dfb9ef90916d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-05-23 09:27:00 +0200

    Testing Pester Tests

---
+++ Testimo.Tests.ps1
@@ -0,0 +1,29 @@
+﻿$PSVersionTable.PSVersion
+
+$ModuleName = (Get-ChildItem $PSScriptRoot\*.psd1).BaseName
+$RequiredModules = @(
+    'PSSharedGoods'
+    @{ Name = 'Pester'; MaximumVersion = '4.99.99.99' }
+)
+foreach ($_ in $RequiredModules) {
+    if ($_ -is [string]) {
+        if ($null -eq (Get-Module -ListAvailable $_)) {
+            Write-Warning "$ModuleName - Downloading $_ from PSGallery"
+            Install-Module -Name $_ -Repository PSGallery -Force -SkipPublisherCheck
+        }
+        Import-Module $_ -Force
+    } else {
+        if ($null -eq (Get-Module -ListAvailable $_.Name)) {
+            Write-Warning "$ModuleName - Downloading $_ from PSGallery"
+            Install-Module -Name $_ -Repository PSGallery -Force -SkipPublisherCheck
+        }
+        Import-Module $_.Name -Force -MaximumVersion $_.MaximumVersion
+    }
+}
+Import-Module $PSScriptRoot\Testimo.psd1 -Force
+
+$result = Invoke-Pester -Script $PSScriptRoot\Tests -Verbose -EnableExit
+
+if ($result.FailedCount -gt 0) {
+    throw "$($result.FailedCount) tests failed."
+}
---
+++ Tests/VerifySources.Tests.ps1
@@ -0,0 +1,11 @@
+﻿Describe 'Testimo Sources' {
+    $TestimoSources = Get-TestimoSources -Advanced
+    foreach ($Source in $TestimoSources) {
+        It "Source $($Source.Source) should contain Enable key and be enabled or disabled" {
+            $Source.Advanced.Enable | Should -BeIn @($True, $False)
+        }
+        It "Source $($Source.Source) should contain Source Key" {
+            $Source.Advanced.Keys | Should -Contain 'Source'
+        }
+    }
+}
