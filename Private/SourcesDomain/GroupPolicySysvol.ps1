9680b9b7551f867888fe9e4e017d1c96f4fd088d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-05-26 19:56:26 +0200

    Update to GroupPolicySysvol

---
+++ Private/SourcesDomain/GroupPolicySysvol.ps1
@@ -0,0 +1,32 @@
+﻿$GroupPolicySysvol = @{
+    Enable = $true
+    Source = @{
+        Name           = "GPO: Sysvol folder existance"
+        Data           = {
+            Get-GPOZaurrSysvol -IncludeDomains $Domain
+        }
+        Details        = [ordered] @{
+            Area        = 'Security'
+            Category    = ''
+            Severity    = ''
+            RiskLevel   = 0
+            Description = "GPO Permissions are stored in Active Directory and SYSVOL at the same time. Sometimes when deleting GPO or due to replication issues GPO becomes orphaned (no SYSVOL files) or SYSVOL files exists but no GPO."
+            Resolution  = ''
+            Resources   = @(
+
+            )
+        }
+        ExpectedOutput = $true
+    }
+    Tests  = [ordered] @{
+        SysvolExists     = @{
+            Enable     = $true
+            Name       = 'GPO: Owner Consistent'
+            Parameters = @{
+                WhereObject    = { $_.SysvolStatus -ne 'Exists' -or $_.Status -ne 'Exists' }
+                ExpectedResult = $false # this tests things in bundle rather then per object of array
+            }
+
+        }
+    }
+}
---
+++ Private/SourcesDomainControllers/GroupPolicySYSVOLDC.ps1
@@ -0,0 +1,19 @@
+﻿$GroupPolicySYSVOLDC = @{
+    Enable = $true
+    Source = @{
+        Name           = "Group Policy SYSVOL Verification"
+        Data           = {
+            Get-GPOZaurrSysvol -IncludeDomains $Domain -IncludeDomainControllers $DomainController -VerifyDomainControllers | Where-Object { $_.SysvolStatus -ne 'Exists' -or $_.Status -ne 'Exists' }
+        }
+        Details        = [ordered] @{
+            Area        = ''
+            Description = ''
+            Resolution  = ''
+            RiskLevel   = 10
+            Resources   = @(
+
+            )
+        }
+        ExpectedOutput = $false
+    }
+}
