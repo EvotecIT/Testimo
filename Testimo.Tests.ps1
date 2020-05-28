bed97073059325721dd13f247bd6395356053b0f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-05-28 11:50:15 +0200

    Update to pester

--- Testimo.Tests.ps1
+++ Testimo.Tests.ps1
@@ -4,26 +4,17 @@ $ModuleName = (Get-ChildItem $PSScriptRoot\*.psd1).BaseName
 $RequiredModules = @(
     'PSSharedGoods'
     'Pester'
-    #@{ Name = 'Pester'; MaximumVersion = '4.99.99.99' }
 )
 foreach ($_ in $RequiredModules) {
-    if ($_ -is [string]) {
-        if ($null -eq (Get-Module -ListAvailable $_)) {
-            Write-Warning "$ModuleName - Downloading $_ from PSGallery"
-            Install-Module -Name $_ -Repository PSGallery -Force -SkipPublisherCheck
-        }
-        Import-Module $_ -Force
-    } else {
-        if ($null -eq (Get-Module -ListAvailable $_.Name)) {
-            Write-Warning "$ModuleName - Downloading $_ from PSGallery"
-            Install-Module -Name $_ -Repository PSGallery -Force -SkipPublisherCheck
-        }
-        Import-Module $_.Name -Force -MaximumVersion $_.MaximumVersion
+    if ($null -eq (Get-Module -ListAvailable $_)) {
+        Write-Warning "$ModuleName - Downloading $_ from PSGallery"
+        Install-Module -Name $_ -Repository PSGallery -Force -SkipPublisherCheck
     }
+    Import-Module $_ -Force
 }
 Import-Module $PSScriptRoot\Testimo.psd1 -Force
 
-$result = Invoke-Pester -Script $PSScriptRoot\Tests -Verbose -EnableExit
+$result = Invoke-Pester -Path $PSScriptRoot\Tests
 
 if ($result.FailedCount -gt 0) {
     throw "$($result.FailedCount) tests failed."
