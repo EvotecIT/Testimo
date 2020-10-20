c59afa66c1cca45a23b73162c935563b046c9ecc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-10-20 08:35:30 +0200

    Update

--- Private/Start-TestimoReport.ps1
+++ Private/Start-TestimoReport.ps1
@@ -23,7 +23,7 @@
     [Array] $SkippedTests = $TestResults['Results'] | Where-Object { $_.Status -ne $true -and $_.Status -ne $false }
 
     New-HTML -FilePath $FilePath -Online:$Online {
-        New-HTMLTableOption -DataStore JavaScript -BoolAsString
+        #New-HTMLTableOption -DataStore JavaScript -BoolAsString
         New-HTMLHeader {
             New-HTMLSection -Invisible {
                 New-HTMLSection {
