bba93fdd378cf6550e0775add6afafd00c41b222
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-10-21 09:22:34 +0200

    Improvement to DCWindowsUpdates

--- Private/SourcesDomainControllers/WindowsUpdates.ps1
+++ Private/SourcesDomainControllers/WindowsUpdates.ps1
@@ -1,16 +1,16 @@
 ﻿$WindowsUpdates = @{
     Enable = $true
     Source = @{
-        Name       = "Windows Updates"
-        Data       = {
-            Get-HotFix -ComputerName $DomainController | Sort-Object -Property InstalledOn -Descending | Select-Object -First 1
+        Name           = "Windows Updates"
+        Data           = {
+            Get-HotFix -ComputerName $DomainController | Sort-Object -Property InstalledOn -Descending | Select-Object -First 1 -Property HotFixID, InstalledOn, Description, InstalledBy
         }
-        Details = [ordered] @{
-            Area             = ''
-            Description      = ''
-            Resolution   = ''
-            RiskLevel        = 10
-            Resources = @(
+        Details        = [ordered] @{
+            Area        = ''
+            Description = ''
+            Resolution  = ''
+            RiskLevel   = 10
+            Resources   = @(
 
             )
         }
@@ -18,9 +18,9 @@
     }
     Tests  = [ordered] @{
         WindowsUpdates = @{
-            Enable      = $true
-            Name        = 'Last Windows Updates should be less than X days ago'
-            Parameters  = @{
+            Enable     = $true
+            Name       = 'Last Windows Updates should be less than X days ago'
+            Parameters = @{
                 Property      = 'InstalledOn'
                 ExpectedValue = '(Get-Date).AddDays(-60)'
                 OperationType = 'gt'
