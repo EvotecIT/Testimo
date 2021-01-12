296ca9853e90d18ba00efb664e7ad6ac96bc77a7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-12 10:54:14 +0100

    Fix for DNSZoneAging

--- Private/SourcesDomain/DNSZonesAging.ps1
+++ Private/SourcesDomain/DNSZonesAging.ps1
@@ -2,17 +2,11 @@
     Enable = $true
     Scope  = 'Domain'
     Source = @{
-        Name    = "Aging primary DNS Zone"
-        Data    = {
-            #$PSWinDocumentationDNS = Import-Module PSWinDocumentation.DNS -PassThru
-            $PSWinDocumentationDNS = Import-PrivateModule PSWinDocumentation.DNS
-            & $PSWinDocumentationDNS {
-                param($Domain)
-                $Zones = Get-WinDnsServerZones -ZoneName $Domain -Domain $Domain
-                Compare-MultipleObjects -Objects $Zones -FormatOutput -CompareSorted:$true -ExcludeProperty GatheredFrom -SkipProperties -Property 'AgingEnabled'
-            } $Domain
+        Name           = "Aging primary DNS Zone"
+        Data           = {
+            Get-WinDNSServerZones -ZoneName $Domain -IncludeDomains $Domain
         }
-        Details = [ordered] @{
+        Details        = [ordered] @{
             Area        = ''
             Category    = ''
             Severity    = ''
@@ -26,25 +20,12 @@
         ExpectedOutput = $true
     }
     Tests  = [ordered] @{
-        EnabledAgingEnabled   = @{
-            Enable      = $true
-            Name        = 'Zone DNS aging should be enabled'
-            # Data     = $Script:SBDomainDnsZonesTestEnabled
-            Parameters  = @{
-                Property      = 'Source'
-                ExpectedValue = $true
-                OperationType = 'eq'
-            }
-            Description = 'Primary DNS zone should have aging enabled.'
-        }
-        EnabledAgingIdentical = @{
+        EnabledAgingEnabledAndIdentical = @{
             Enable      = $true
             Name        = 'Zone DNS aging should be identical on all DCs'
-            #Data     = $Script:SBDomainDnsZonesTestIdentical
             Parameters  = @{
-                Property      = 'Status'
-                ExpectedValue = $true
-                OperationType = 'eq'
+                WhereObject   = { $_.AgingEnabled -eq $false }
+                ExpectedCount = 0
             }
             Description = 'Primary DNS zone should have aging enabled, on all DNS servers.'
         }
