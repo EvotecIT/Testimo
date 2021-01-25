5fda34d58824fbe1f9bff86d87f542b667424f53
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-25 16:47:46 +0100

    RiskLevels basics

---
+++ Private/RiskLevels.ps1
@@ -0,0 +1,38 @@
+$Script:RiskLevel = @{
+    0  = 'Informational (0)'
+    1  = 'Neglible (1)'
+    2  = 'Very low (2)'
+    3  = 'Low (3)'
+    4  = 'Minor (4)'
+    5  = 'Moderate Low (5)'
+    6  = 'Moderate (6)'
+    7  = 'High (7)'
+    8  = 'Very High (8)'
+    9  = 'Signiciant (9)'
+    10 = 'Extream (10)'
+}
+$Script:RiskType = @{
+    0 = 'Informational'
+    1 = 'Recommended'
+    2 = 'Must Have'
+}
+$Script:Impact = @{
+    0 = 'Neglible'
+    1 = 'Minor'
+    3 = 'Moderate'
+    4 = 'Significant'
+    5 = 'Severe'
+}
+$Script:Likelihood = @{
+    0 = 'Very Unlikely'
+    1 = 'Unlikely'
+    2 = 'Possible'
+    3 = 'Likely'
+    4 = 'Very Likely'
+}
+$Script:Consequence = @{
+    0 = 'Small'
+    1 = 'Moderate'
+    2 = 'Severe'
+    3 = 'Catastrophic'
+}
