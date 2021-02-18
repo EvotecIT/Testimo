638d47d0294154b86d78129b5843a4f82a63dabf
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-18 10:07:19 +0100

    Improve LDAP

--- Private/SourcesDomainControllers/Ldap.ps1
+++ Private/SourcesDomainControllers/Ldap.ps1
@@ -2,23 +2,23 @@
     Enable = $true
     Scope  = 'DC'
     Source = @{
-        Name       = 'LDAP Connectivity'
-        Data       = {
-            Test-LDAP -ComputerName $DomainController -WarningAction SilentlyContinue
+        Name           = 'LDAP Connectivity'
+        Data           = {
+            Test-LDAP -ComputerName $DomainController -WarningAction SilentlyContinue -VerifyCertificate
         }
-        Details = [ordered] @{
-            Area             = ''
-            Description      = ''
-            Resolution   = ''
-            Importance        = 10
-            Resources = @(
+        Details        = [ordered] @{
+            Area        = ''
+            Description = ''
+            Resolution  = ''
+            Importance  = 10
+            Resources   = @(
 
             )
         }
         ExpectedOutput = $true
     }
     Tests  = [ordered] @{
-        PortLDAP     = @{
+        PortLDAP      = @{
             Enable     = $true
             Name       = 'LDAP Port is Available'
 
@@ -28,7 +28,7 @@
                 OperationType = 'eq'
             }
         }
-        PortLDAPS    = @{
+        PortLDAPS     = @{
             Enable     = $true
             Name       = 'LDAP SSL Port is Available'
             Parameters = @{
@@ -37,7 +37,7 @@
                 OperationType = 'eq'
             }
         }
-        PortLDAP_GC  = @{
+        PortLDAP_GC   = @{
             Enable     = $true
             Name       = 'LDAP GC Port is Available'
             Parameters = @{
@@ -46,13 +46,49 @@
                 OperationType = 'eq'
             }
         }
-        PortLDAPS_GC = @{
+        PortLDAPS_GC  = @{
             Enable     = $true
             Name       = 'LDAP SSL GC Port is Available'
             Parameters = @{
                 Property      = 'GlobalCatalogLDAPS'
                 ExpectedValue = $true
                 OperationType = 'eq'
+            }
+        }
+        BindLDAPS     = @{
+            Enable     = $true
+            Name       = 'LDAP SSL Bind available'
+            Parameters = @{
+                Property      = 'LDAPSBind'
+                ExpectedValue = $true
+                OperationType = 'eq'
+            }
+        }
+        BindLDAPS_GC  = @{
+            Enable     = $true
+            Name       = 'LDAP SSL GC Bind is Available'
+            Parameters = @{
+                Property      = 'GlobalCatalogLDAPSBind'
+                ExpectedValue = $true
+                OperationType = 'eq'
+            }
+        }
+        X509NotBefore = @{
+            Enable     = $true
+            Name       = 'Not Before Date should be within required range'
+            Parameters = @{
+                Property      = 'X509NotBefore'
+                ExpectedValue = Get-Date
+                OperationType = 'lt'
+            }
+        }
+        X509NotAfter  = @{
+            Enable     = $true
+            Name       = 'Not After Date should bee within required range'
+            Parameters = @{
+                Property      = 'X509NotAfter'
+                ExpectedValue = Get-Date
+                OperationType = 'gt'
             }
         }
     }
