6a32a40858faca13598c9813681b87f753369ada
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-24 14:31:20 +0100

    Improved risk levels

--- Private/RiskLevels.ps1
+++ Private/RiskLevels.ps1
@@ -1,33 +1,45 @@
 $Script:Importance = @{
-    0  = 'Informational (0)'
-    1  = 'Neglible (1)'
-    2  = 'Very low (2)'
-    3  = 'Low (3)'
-    4  = 'Minor (4)'
-    5  = 'Moderate Low (5)'
-    6  = 'Moderate (6)'
-    7  = 'High (7)'
-    8  = 'Very High (8)'
-    9  = 'Signiciant (9)'
-    10 = 'Extream (10)'
+    0  = 'Informational'
+    1  = 'Neglible)'
+    2  = 'Very low'
+    3  = 'Low'
+    4  = 'Minor'
+    5  = 'Moderate Low'
+    6  = 'Moderate'
+    7  = 'High'
+    8  = 'Very High'
+    9  = 'Signiciant'
+    10 = 'Extream'
 }
 $Script:StatusTranslation = @{
-    0 = 'Low' # #4D9F6F # Low risk
-    1 = 'Guarded' # #507DC6 # General Risk
-    2 = 'Elevated' # #998D16 # Significant Risk
-    3 = 'High' # #7A5928 High Risk
-    4 = 'Severe' # #D65742 Server Risk
+    -1 = 'Skipped'
+    0  = 'Informational' # #4D9F6F # Low risk
+    1  = 'Good'
+    2  = 'Low' # #507DC6 # General Risk
+    3  = 'Elevated' # #998D16 # Significant Risk
+    4  = 'High' # #7A5928 High Risk
+    5  = 'Severe' # #D65742 Server Risk
 }
-$Script:WarningSystem = @{
-    0 = 'All Clear'
-    1 = 'Advice'
-    2 = 'Watch and Act'
-    3 = 'Emergency Warning'
+$Script:StatusTranslationColors = @{
+    -1 = 'DeepSkyBlue'
+    0  = 'ElectricBlue'
+    1  = 'LawnGreen'
+    2  = 'ParisDaisy' #  # General Risk
+    3  = 'SafetyOrange' #  # Significant Risk
+    4  = 'InternationalOrange' #  High Risk
+    5  = 'TorchRed' #  Server Risk
 }
 $Script:ActionType = @{
     0 = 'Informational'
     1 = 'Recommended'
     2 = 'Must Implement'
+}
+<#
+$Script:WarningSystem = @{
+    0 = 'All Clear'
+    1 = 'Advice'
+    2 = 'Watch and Act'
+    3 = 'Emergency Warning'
 }
 $Script:PotentialImpact = @{
     0 = 'Neglible'
@@ -49,3 +61,4 @@ $Script:Consequence = @{
     2 = 'Severe'
     3 = 'Catastrophic'
 }
+#>
