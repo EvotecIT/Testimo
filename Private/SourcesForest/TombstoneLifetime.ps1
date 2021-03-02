f8f43b8b7a612f7f5c5f9e0d56a68307c9477b30
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-02 11:47:12 +0100

    Improved Tombstonelifetime

--- Private/SourcesForest/TombstoneLifetime.ps1
+++ Private/SourcesForest/TombstoneLifetime.ps1
@@ -1,7 +1,7 @@
 ﻿$TombstoneLifetime = @{
-    Enable    = $true
-    Scope     = 'Forest'
-    Source    = [ordered]@{
+    Enable         = $true
+    Scope          = 'Forest'
+    Source         = [ordered]@{
         Name           = 'Tombstone Lifetime'
         Data           = {
             # Check tombstone lifetime (if blank value is 60)
@@ -17,20 +17,21 @@
             }
         }
         Details        = [ordered] @{
-            Area        = 'Configuration'
-            Category    = 'Backup'
-            Description = ''
-            Resolution  = ''
-            Importance   = 10
-            Severity    = 'High'
+            Category    = 'Configuration'
+            Description = "A tombstone is a container object consisting of the deleted objects from AD. These objects have not been physically removed from the database. When an AD object, such as a user is deleted, the object technically remains in the directory for a given period of time; known as the Tombstone Lifetime.  At that point, Active Directory sets the ‘isDeleted' attribute of the deleted object to TRUE and moves it to a special container called Tombstone (previously known as CN=Deleted Objects.)  Once the object is older than the tombstone lifetime, it will be removed (physically deleted) by the garbage collection process."
+            Importance  = 0
+            ActionType  = 0
             Resources   = @(
-
+                '[Understanding Tombstones, Active Directory, and How To Protect It](https://support.storagecraft.com/s/article/Understanding-Tombstones-Active-Directory-and-How-To-Protect-It?language=en_US)'
+                '[Adjust Active Directory Tombstone Lifetime](https://helpcenter.netwrix.com/NA/Configure_IT_Infrastructure/AD/AD_Tombstone.html)'
             )
+            StatusTrue  = 0
+            StatusFalse = 2
         }
         ExpectedOutput = $true
     }
-    Tests     = [ordered] @{
-        TombstoneLifetime = @{
+    Tests          = [ordered] @{
+        TombstoneLifetime = [ordered] @{
             Enable     = $true
             Name       = 'TombstoneLifetime should be set to minimum of 180 days'
             Parameters = @{
@@ -38,9 +39,18 @@
                 Property      = 'TombstoneLifeTime'
                 OperationType = 'ge'
             }
+            Details    = [ordered] @{
+                Category    = 'Configuration'
+                Importance  = 7
+                ActionType  = 2
+                StatusTrue  = 1
+                StatusFalse = 3
+            }
         }
     }
-    Resources = @(
-        'https://helpcenter.netwrix.com/Configure_IT_Infrastructure/AD/AD_Tombstone.html'
-    )
+    DataHighlights = {
+        New-HTMLTableCondition -Name 'tombstoneLifetime' -ComparisonType number -BackgroundColor PaleGreen -Value 180 -Operator ge -Row
+        New-HTMLTableCondition -Name 'tombstoneLifetime' -ComparisonType number -BackgroundColor Orange -Value 180 -Operator lt -Row
+        New-HTMLTableCondition -Name 'tombstoneLifetime' -ComparisonType number -BackgroundColor Salmon -Value 60 -Operator le -Row
+    }
 }
