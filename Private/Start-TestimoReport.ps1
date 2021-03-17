d401bac9281b022524aa00e1beebd76c72db9ce9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-17 10:40:35 +0100

    Improved support for new PSWriteHTML

--- Private/Start-TestimoReport.ps1
+++ Private/Start-TestimoReport.ps1
@@ -39,15 +39,30 @@
         New-HTMLTableCondition -Name 'Assesment' -Value $true -BackgroundColor $TestResults['Configuration']['Colors']['ColorPassed'] -Color $TestResults['Configuration']['Colors']['ColorPassedText'] #-Row
         New-HTMLTableCondition -Name 'Assesment' -Value $false -BackgroundColor $TestResults['Configuration']['Colors']['ColorFailed'] -Color $TestResults['Configuration']['Colors']['ColorFailedText'] #-Row
     }
-    $TestResults['Configuration']['ResultConditionsEmail'] = & {
-        #New-TableCondition -Name 'Status' -Value $true -BackgroundColor 'LawnGreen' -Inline
-        # New-TableCondition -Name 'Status' -Value $false -BackgroundColor 'Tomato' -Inline
-        #New-TableCondition -Name 'Status' -Value $null -BackgroundColor 'DeepSkyBlue' -Inline
-        foreach ($Status in $Script:StatusTranslation.Keys) {
-            New-HTMLTableCondition -Name 'Assesment' -Value $Script:StatusTranslation[$Status] -BackgroundColor $Script:StatusTranslationColors[$Status] -Inline
+    $TestResults['Configuration']['ResultConditionsEmail'] = {
+        $Translations = @{
+            -1 = 'Skipped'
+            0  = 'Informational' # #4D9F6F # Low risk
+            1  = 'Good'
+            2  = 'Low' # #507DC6 # General Risk
+            3  = 'Elevated' # #998D16 # Significant Risk
+            4  = 'High' # #7A5928 High Risk
+            5  = 'Severe' # #D65742 Server Risk
         }
-        New-HTMLTableCondition -Name 'Assesment' -Value $true -BackgroundColor $TestResults['Configuration']['Colors']['ColorPassed'] -Color $TestResults['Configuration']['Colors']['ColorPassedText'] -Inline
-        New-HTMLTableCondition -Name 'Assesment' -Value $false -BackgroundColor $TestResults['Configuration']['Colors']['ColorFailed'] -Color $TestResults['Configuration']['Colors']['ColorFailedText'] -Inline
+        $TranslationsColors = @{
+            -1 = 'DeepSkyBlue'
+            0  = 'ElectricBlue'
+            1  = 'LawnGreen'
+            2  = 'ParisDaisy' #  # General Risk
+            3  = 'SafetyOrange' #  # Significant Risk
+            4  = 'InternationalOrange' #  High Risk
+            5  = 'TorchRed' #  Server Risk
+        }
+        foreach ($Status in $Translations.Keys) {
+            New-HTMLTableCondition -Name 'Assesment' -Value $Translations[$Status] -BackgroundColor $TranslationsColors[$Status] -Inline
+        }
+        New-HTMLTableCondition -Name 'Assesment' -Value $true -BackgroundColor 'LawnGreen' -Inline
+        New-HTMLTableCondition -Name 'Assesment' -Value $false -BackgroundColor 'TorchRed' -Inline
     }
     [Array] $PassedTests = $TestResults['Results'] | Where-Object { $_.Status -eq $true }
     [Array] $FailedTests = $TestResults['Results'] | Where-Object { $_.Status -eq $false }
@@ -87,9 +102,9 @@
                             New-ChartPie -Name 'Skipped' -Value ($SkippedTests.Count) -Color $ColorSkipped
                         }
                         New-HTMLTable -DataTable $TestResults['Summary'] -HideFooter -DisableSearch {
-                            New-HTMLTableContent -ColumnName 'Passed' -BackGroundColor $TestResults['Configuration']['Colors']['ColorPassed'] -Color $TestResults['Configuration']['Colors']['ColorPassedText']
-                            New-HTMLTableContent -ColumnName 'Failed' -BackGroundColor $TestResults['Configuration']['Colors']['ColorFailed'] -Color $TestResults['Configuration']['Colors']['ColorFailedText']
-                            New-HTMLTableContent -ColumnName 'Skipped' -BackGroundColor $TestResults['Configuration']['Colors']['ColorSkipped'] -Color $TestResults['Configuration']['Colors']['ColorSkippedText']
+                            New-HTMLTableContent -ColumnName 'Passed' -BackgroundColor $TestResults['Configuration']['Colors']['ColorPassed'] -Color $TestResults['Configuration']['Colors']['ColorPassedText']
+                            New-HTMLTableContent -ColumnName 'Failed' -BackgroundColor $TestResults['Configuration']['Colors']['ColorFailed'] -Color $TestResults['Configuration']['Colors']['ColorFailedText']
+                            New-HTMLTableContent -ColumnName 'Skipped' -BackgroundColor $TestResults['Configuration']['Colors']['ColorSkipped'] -Color $TestResults['Configuration']['Colors']['ColorSkippedText']
                         } -DataStore HTML -Buttons @() -DisablePaging
                     } -Width '35%'
                     New-HTMLContainer {
