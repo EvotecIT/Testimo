a9d2f703f5c2bdad15eb377fecbb5aff2d5db776
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-07 16:43:32 +0200

    Update

--- Example/Example-QuickTest.ps1
+++ Example/Example-QuickTest.ps1
@@ -1,20 +1,36 @@
 ﻿Import-Module .\Testimo.psd1 -Force
 
 $Sources = @(
-    'ForestBackup'
-    #'ForestConfigurationPartitionOwners'
-    #'ForestConfigurationPartitionOwnersContainers'
-    #'ForestOptionalFeatures'
-    #'ForestOrphanedAdmins'
-    #'DomainDuplicateObjects'
-    #'DomainDomainControllers'
-    'DomainLDAP'
+
+    # 'ForestTombstoneLifetime'
+    # 'ForestOptionalFeatures'
+    # 'ForestBackup'
+    # 'ForestConfigurationPartitionOwners'
+    # 'ForestConfigurationPartitionOwnersContainers'
+    # 'ForestOptionalFeatures'
+    # 'ForestOrphanedAdmins'
+    # 'DomainDuplicateObjects'
+    # 'DomainDomainControllers'
+    # 'DomainLDAP'
+    # 'ForestSites'
+    # 'ForestSubnets'
+    # 'DomainOrphanedForeignSecurityPrincipals'
+    # 'ForestTrusts'
+    #'DomainSecurityUsers'
+    #'DomainSecurityKRBGT'
+    # 'ForestTrusts'
+    # 'ForestRoles'
+    'DomainSecurityComputers'
+    #'DomainWellKnownFolders'
+    #'DomainSecurityUsersAcccountAdministrator'
+    #'DomainSecurityGroupsSchemaAdmins'
+    #'DomainSecurityGroupsAccountOperators'
 )
 
 # Tests one by one
 foreach ($_ in $Sources) {
-    # Invoke-Testimo -Source $_ -Online #-ReportPath $PSScriptRoot\Reports\TestimoSummary.html
+    #Invoke-Testimo -Source $_ -Online -ReportPath "$PSScriptRoot\Reports\Testimo_$($_).html" -AlwaysShowSteps
 }
 
 # Tests in single report
-Invoke-Testimo -Source $Sources -Online -ReportPath $PSScriptRoot\Reports\TestimoSummary.html
+Invoke-Testimo -Source $Sources -Online -ReportPath $PSScriptRoot\Reports\TestimoSummary.html -AlwaysShowSteps #-HideHTML
