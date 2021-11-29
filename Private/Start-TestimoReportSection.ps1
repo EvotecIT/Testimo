20880a8d2f284402b5e112711488249470b6180a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-11-29 22:39:38 +0100

    Fixes a potential issue with null values and charts.

--- Private/Start-TestimoReportSection.ps1
+++ Private/Start-TestimoReportSection.ps1
@@ -34,13 +34,24 @@ function Start-TestimoReportSection {
 
     $ChartData = [ordered] @{}
     foreach ($Result in $Results) {
-        if (-not $ChartData[$Result.Assessment]) {
-            $ChartData[$Result.Assessment] = [ordered] @{
-                Count = 0
-                Color = $Script:StatusToColors[$Result.Assessment]
+        if ($null -ne $Result.Assessment) {
+            if (-not $ChartData[$Result.Assessment]) {
+                $ChartData[$Result.Assessment] = [ordered] @{
+                    Count = 0
+                    Color = $Script:StatusToColors[$Result.Assessment]
+                }
             }
+            $ChartData[$Result.Assessment].Count++
+        } else {
+            # if for whatever reason result.assesment is null we need to improvise
+            if (-not $ChartData['Skipped']) {
+                $ChartData['Skipped'] = [ordered] @{
+                    Count = 0
+                    Color = $Script:StatusToColors['Skipped']
+                }
+            }
+            $ChartData['Skipped'].Count++
         }
-        $ChartData[$Result.Assessment].Count++
     }
 
 
