6b74e1f6255558d1793df4e7706bdd6717361954
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-12-03 08:37:25 +0100

    Improve #152

--- Private/SourcesDomain/OrganizationalUnitsEmpty.ps1
+++ Private/SourcesDomain/OrganizationalUnitsEmpty.ps1
@@ -4,6 +4,10 @@
     Source = @{
         Name           = "Organizational Units: Orphaned/Empty"
         Data           = {
+            <# We should replace it with better alternative
+            ([adsisearcher]'(objectcategory=organizationalunit)').FindAll() | Where-Object {
+            -not (-join $_.GetDirectoryEntry().psbase.children) }
+            #>
             $OrganizationalUnits = Get-ADOrganizationalUnit -Filter * -Properties distinguishedname -Server $Domain | Select-Object -ExpandProperty distinguishedname
             $WellKnownContainers = Get-ADDomain | Select-Object *Container
 
@@ -15,15 +19,16 @@
             $OrganizationalUnits | Where-Object { ($AllUsedOU -notcontains $_) -and -not (Get-ADOrganizationalUnit -Filter * -SearchBase $_ -SearchScope 1 -Server $Domain) -and (($_ -notlike $WellKnownContainers.UsersContainer) -or ($_ -notlike $WellKnownContainers.ComputersContainer)) }
         }
         Details        = [ordered] @{
-            Area        = ''
-            Category    = ''
-            Severity    = ''
-            Importance   = 0
+            Category    = 'Configuration'
+            Importance  = 3
+            ActionType  = 1
             Description = ''
             Resolution  = ''
             Resources   = @(
-
+                "[Active Directory Friday: Find empty Organizational Unit](https://www.jaapbrasser.com/active-directory-friday-find-empty-organizational-unit/)"
             )
+            StatusTrue  = 1
+            StatusFalse = 2
         }
         ExpectedOutput = $false
     }
