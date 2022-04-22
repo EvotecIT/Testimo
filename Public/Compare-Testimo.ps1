43b3c9396c5871682c4b1b5c6e2b69ac3f138c64
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-04-22 08:25:29 +0200

    Improvement for DSC testing

--- Public/Compare-Testimo.ps1
+++ Public/Compare-Testimo.ps1
@@ -27,7 +27,7 @@
         [string[]] $ExcludeProperty
     )
 
-
+    $IsDsc = $false
     if ($PSBoundParameters.ContainsKey("BaseLineSourcePath")) {
         $SourcePath = Get-Item -LiteralPath $BaseLineSourcePath -ErrorAction SilentlyContinue
         $TargetPath = Get-Item -LiteralPath $BaseLineTargetPath -ErrorAction SilentlyContinue
@@ -38,11 +38,61 @@
         if ($SourcePath.Extension -eq '.json' -and $TargetPath.Extension -eq '.json') {
             $BaseLineSource = Get-Content -LiteralPath $BaseLineSourcePath -Raw | ConvertFrom-Json
             $BaseLineTarget = Get-Content -LiteralPath $BaseLineTargetPath -Raw | ConvertFrom-Json
+            @{
+                Name            = $Name
+                DisplayName     = if ($DisplayName) { $DisplayName } else { $Name }
+                Scope           = $Scope
+                Category        = $Category
+                BaseLineSource  = $BaseLineSource
+                BaseLineTarget  = $BaseLineTarget
+                ExcludeProperty = $ExcludeProperty
+            }
         } elseif ($SourcePath.Extension -eq '.ps1' -and $TargetPath.Extension -eq '.ps1') {
+            $IsDsc = $true
             $CommandExists = Get-Command -Name 'ConvertTo-DSCObject' -ErrorAction SilentlyContinue
             if ($CommandExists) {
+                $DSCGroups = [ordered] @{}
+                $DSCGroupsTarget = [ordered] @{}
+
                 $BaseLineSource = ConvertTo-DSCObject -Path $BaseLineSourcePath
                 $BaseLineTarget = ConvertTo-DSCObject -Path $BaseLineTargetPath
+
+                foreach ($DSC in $BaseLineSource) {
+                    if (-not $DSCGroups[$DSC.ResourceName]) {
+                        $DSCGroups[$DSC.ResourceName] = [System.Collections.Generic.List[PSCustomObject]]::new()
+                    }
+                    $DSCGroups[$DSC.ResourceName].Add([PSCustomObject] $DSC)
+                }
+                foreach ($DSC in $BaseLineTarget) {
+                    if (-not $DSCGroupsTarget[$DSC.ResourceName]) {
+                        $DSCGroupsTarget[$DSC.ResourceName] = [System.Collections.Generic.List[PSCustomObject]]::new()
+                    }
+                    $DSCGroupsTarget[$DSC.ResourceName].Add([PSCustomObject] $DSC)
+                }
+
+                foreach ($Source in $DSCGroups.Keys) {
+                    if ($DSCGroupsTarget[$Source]) {
+                        @{
+                            Name            = $Source
+                            DisplayName     = $Source
+                            Scope           = $Scope
+                            Category        = $Category
+                            BaseLineSource  = if ($DSCGroups[$Source].Count -eq 1) { $DSCGroups[$Source][0] } else { $DSCGroups[$Source] }
+                            BaseLineTarget  = if ($DSCGroupsTarget[$Source].Count -eq 1) { $DSCGroupsTarget[$Source][0] } else { $DSCGroupsTarget[$Source] }
+                            ExcludeProperty = $ExcludeProperty
+                        }
+                    } else {
+                        @{
+                            Name            = $Source
+                            DisplayName     = $Source
+                            Scope           = $Scope
+                            Category        = $Category
+                            BaseLineSource  = if ($DSCGroups[$Source].Count -eq 1) { $DSCGroups[$Source][0] } else { $DSCGroups[$Source] }
+                            BaseLineTarget  = $null
+                            ExcludeProperty = $ExcludeProperty
+                        }
+                    }
+                }
             } else {
                 Out-Informative -Text "DSCParser is not available. Skipping source $Name" -Level 0 -Status $null -ExtendedValue $null
             }
@@ -54,14 +104,5 @@
             Out-Informative -Text "Loading BaseLineSource or BaseLineTarget didn't work. Skipping source $Name" -Level 0 -Status $null -ExtendedValue $null
             return
         }
-    }
-    @{
-        Name            = $Name
-        DisplayName     = if ($DisplayName) { $DisplayName } else { $Name }
-        Scope           = $Scope
-        Category        = $Category
-        BaseLineSource  = $BaseLineSource
-        BaseLineTarget  = $BaseLineTarget
-        ExcludeProperty = $ExcludeProperty
     }
 }
