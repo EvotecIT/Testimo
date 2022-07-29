5066e85d1d04fc272288c06ccb4479127ca0cb24
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-29 12:51:12 +0200

    Fixes issues with null output

--- Private/Test-StepTwo.ps1
+++ Private/Test-StepTwo.ps1
@@ -40,7 +40,7 @@
         foreach ($V in $Property) {
             $TestedValue = $TestedValue.$V
         }
-        if ($TestedValue.GetType().BaseType.Name -eq 'Enum') {
+        if ($null -ne $TestedValue -and $TestedValue.GetType().BaseType.Name -eq 'Enum') {
             $TestedValue = $TestedValue.ToString()
         }
 
